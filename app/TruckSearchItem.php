9ed6253a925e9817d39f8a0460ca0e845b8c9e1a
trubinov <mrtrubinov@gmail.com>
2020-01-27 18:20:59 +0500

    В результате поиска машин выдается 50 записей

--- app/TruckSearchItem.php
+++ app/TruckSearchItem.php
@@ -9,6 +9,8 @@ class TruckSearchItem extends Model
 
     public $incrementing = false;
 
+    protected $perPage = 50;
+
     protected $fillable = [
         'subscriber_id', 'available_date', 'body_type', 'weight', 'volume',
         'city_code', 'city_name', 'city_latitude', 'city_longitude',
